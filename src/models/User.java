98bb3a9af2ea3d95459df7281afb477021b7d61e
Joaquin Strusiat <joaquinpjs@gmail.com>
2024-10-28 14:58:56 -0300

    28/10 - 14:58 - model user

--- src/models/User.java
+++ src/models/User.java
@@ -1,5 +1,12 @@
 package models;
 
 public class User {
+    private int id;
+    private String name;
+    private String last_name;
+    private String email;
+    private String country;
+    private String city;
+    private int dni;
     
 }

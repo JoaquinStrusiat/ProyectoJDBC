b8123e17e761a68c9695dd4fb110191ec5f0d9a4
Joaquin Strusiat <joaquinpjs@gmail.com>
2024-10-29 18:50:27 -0300

    29/10 - 18:50

--- src/conection/Conection.java
+++ src/conection/Conection.java
@@ -12,18 +12,22 @@ public class Conection {
     public static void main(String[] args) {
 
         //Importo las variables de configuración
-        HashMap<String, String> VarList = env.getEnvVar("dev");
+        HashMap<String, String> varList = env.getEnvVar("dev");
 
         /*
         Esta parte es solo para entendér el código, luego hay que eliminarla y pasar
         las variables directamente al conector
         */ 
-        String url = VarList.get("dbUrl");  
-        String username = VarList.get("dbUsername"); 
-        String password = VarList.get("dbPassword");
+        String url = varList.get("dbUrl");  
+        String username = varList.get("dbUsername"); 
+        String password = varList.get("dbPassword");
+
+        System.out.println(url);
+        System.out.println(username);
+        System.out.println(password);
 
         String query = "SELECT * FROM user";
-
+         
         try (
             Connection conect = DriverManager.getConnection(url, username, password);
             Statement statement = conect.createStatement();

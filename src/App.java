3c94d8480a44d228c18ad8fc0a2866542df06bd0
Joaquin Strusiat <joaquinpjs@gmail.com>
2024-10-30 02:20:21 -0300

    30/10 - 2:20 - Correción de ConectionDB

--- src/App.java
+++ src/App.java
@@ -1,10 +1,7 @@
 // Acá solo se inicializan los módulos
-import java.util.HashMap;
-import utils.env;
-
 public class App {
     public static void main(String[] args) {
-        HashMap<String, String> VarList = env.getEnvVar("");
+        
     }
     
 }
--- src/conection/ConectionDB.java
+++ src/conection/ConectionDB.java
@@ -1,50 +1,86 @@
 package conection;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
-
 import utils.env;
 
 public class ConectionDB {
-    //Cambiar esta línea para definir que variables usar
-    private static String fileEnv = "dev";
-    //Linea de arriba
-    
+    private String url;
+    private String user;
+    private String password;
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
 
-    public static Statement createConection(){
-        HashMap<String, String> varList = env.getEnvVar(fileEnv);
-        try (
-            Connection conect = DriverManager.getConnection(varList.get("dbUrl"), varList.get("dbUser"), varList.get("dbPassword"));
-            Statement statement = conect.createStatement();
-            ) {
-                System.out.println("Conexion establecida: " + conect);
-                return statement;  
+    public ConectionDB(String file){
+        //Obtengo por parámetros las variables a usar
+        HashMap<String, String> varList = env.getEnvVar(file);
+        this.url = varList.get("dbUrl");
+        this.user = varList.get("dbUsername");
+        this.password = varList.get("dbPassword");
+        try {
+            this.connection = DriverManager.getConnection(this.url, this.user, this.password);
+            this.statement = connection.createStatement();
         } catch (SQLException e) {
-            System.out.println(e);
-            return null;
+            System.out.println("Error de conexion: " + e);
         }
     }
 
-    public static void main(String[] args) {
-
-        String query = "SELECT * FROM user";
-
-        try (
-            Statement stc = ConectionDB.createConection();
-            ResultSet resultSet = stc.executeQuery(query);
-            ) {
-                while (resultSet.next()) {
-                    String nombre = resultSet.getString("name");
-                    String dni = resultSet.getString("dni");
-                    String email = resultSet.getString("email");
-                    System.out.println("Nombre: " + nombre + " DNI: " + dni  + " Email: " + email);
-                }
-            
+    public void closeConectionDB() {
+        try {
+            if (statement != null && !statement.isClosed()) {
+                statement.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
         } catch (SQLException e) {
             System.out.println(e);
+        }  
+    }
+
+    public ResultSet executeQuery(String query){
+        try {
+            resultSet = statement.executeQuery(query);
+        } catch (SQLException e) {
+            System.out.println("Error de consulta: " + e);
         }
+        return resultSet;
+    }
+
+    public String executeUpdate(String query){
+        try {
+            int update = statement.executeUpdate(query);
+            String updatetoString = Integer.toString(update);
+            return "Número de actualizaciones: " + updatetoString;
+        } catch (Exception e) {
+            return "Error de actualizacion: " + e;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        //Declaro la ruta de variables
+        String routeEnv = "dev";
+        //Declaro la consulta sql
+        String query = "SELECT * FROM user";
+        //Inicializo una conección con la ruta de variables a implementar
+        ConectionDB db = new ConectionDB(routeEnv);
+        //Manejo de la respuesta
+        try (ResultSet resultSet = db.executeQuery(query)) {
+            while (resultSet.next()) {
+                String nombre = resultSet.getString("name");
+                String dni = resultSet.getString("dni");
+                String email = resultSet.getString("email");
+                System.out.println("Nombre: " + nombre + " DNI: " + dni  + " Email: " + email);
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        db.closeConectionDB();        
     }
 }

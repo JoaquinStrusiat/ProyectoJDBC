c32af0ac60de5f4d40fa63fdd166f1ab95337074
Juan Pablo Lenain <122615545+JPjuanpablolenain@users.noreply.github.com>
2024-11-05 17:09:15 -0300

    Update Category.java
    Cree la tabla Category

--- src/models/Category.java
+++ src/models/Category.java
@@ -4,21 +4,46 @@ import java.util.Scanner;
 
 public class Category {
     private String name;
+    private int category; // clave y clave foránea para conectar con tabla de productos
+    private String description;
 
-    private Category(String name){
+    private Category(String name, int category, String discription){
         this.name = name;
+        this.category = category;
+        this.description = discription;
     }
     
     public static Category createCategory(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese el nombre de la Categoría a Crear: ");
-        String catName = scanner.nextLine();
-        Category cat = new Category(catName);
-        scanner.close();
+        Category cat;
+        while(true){
+            try{
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Ingrese el nombre de la Categoría a Crear: ");
+                String catName = scanner.nextLine();
+
+                System.out.println("Ingrese el ID de la categoría: ");
+                int catID = scanner.nextInt();
+
+                System.out.println("Ingrese descripción: ");
+                String catDescription = scanner.nextLine();
+
+                scanner.close();
+                cat = new Category(catName, catID, catDescription);
+                break;
+            } catch (Exception e) {
+                System.out.println("\nEntrada no valida, ingrese nuevamente los datos: ");
+            }
+        }
         return cat;
     }
 
     public String getName() {
         return name;
+    }
+    public int getCategoryID(){
+        return category;
+    }
+    public String getDescription(){
+        return description;
     }
 }

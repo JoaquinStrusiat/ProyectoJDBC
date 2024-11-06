b42634185b17bf8e35917f349033ef5334e47b0b
Joaquin Strusiat <joaquinpjs@gmail.com>
2024-11-05 22:32:14 -0300

    5/11 - 22:31 - modelo cartegoria arreglado

--- src/models/Category.java
+++ src/models/Category.java
@@ -4,31 +4,21 @@ import java.util.Scanner;
 
 public class Category {
     private String name;
-    private int category; // clave y clave foránea para conectar con tabla de productos
-    private String description;
 
-    private Category(String name, int category, String discription){
+    private Category(String name){
         this.name = name;
-        this.category = category;
-        this.description = discription;
     }
-    
+
     public static Category createCategory(){
         Category cat;
         while(true){
             try{
                 Scanner scanner = new Scanner(System.in);
-                System.out.println("Ingrese el nombre de la Categoría a Crear: ");
+                System.out.print("Ingrese el nombre de la Categoría a Crear: ");
                 String catName = scanner.nextLine();
 
-                System.out.println("Ingrese el ID de la categoría: ");
-                int catID = scanner.nextInt();
-
-                System.out.println("Ingrese descripción: ");
-                String catDescription = scanner.nextLine();
-
                 scanner.close();
-                cat = new Category(catName, catID, catDescription);
+                cat = new Category(catName);
                 break;
             } catch (Exception e) {
                 System.out.println("\nEntrada no valida, ingrese nuevamente los datos: ");
@@ -36,3 +26,8 @@ public class Category {
         }
         return cat;
     }
+
+    public String getName() {
+        return name;
+    }
+}

e0a4bc4aa3cbd06f994f9c25d1866c5ced29eef5
Joaquin Strusiat <joaquinpjs@gmail.com>
2024-11-08 12:26:18 -0300

    8/11 - 12:26

--- src/controllers/userControls.java
+++ src/controllers/userControls.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import conection.ConectionDB;
 import models.User;
@@ -17,15 +16,12 @@ public class userControls {
     
         // Crear una instancia de la conexión
         ConectionDB db = new ConectionDB();
+
+        // Declaro la query
+        String query = "INSERT INTO users (name, last_name, email, country, city, dni, password) VALUES (?, ?, ?, ?, ?, ?, ?)";
     
         // Ejecutar la inserción usando PreparedStatement
-        try (
-            PreparedStatement stmtUser = db.executeChange(
-                "INSERT INTO users (name, last_name, email, country, city, dni) VALUES (?, ?, ?, ?, ?, ?)",
-                Statement.RETURN_GENERATED_KEYS // Permite obtener las claves generadas automáticamente
-            );
-            PreparedStatement stmPws = db.executeChange("INSERT INTO passwords (id_usuario, password) VALUES (?, ?)")
-        ) {
+        try (PreparedStatement stmtUser = db.executeChange(query)) {
             // guardo al usuario
             stmtUser.setString(1, newUser.getName());
             stmtUser.setString(2, newUser.getLast_name());
@@ -33,27 +29,13 @@ public class userControls {
             stmtUser.setString(4, newUser.getCountry());
             stmtUser.setString(5, newUser.getCity());
             stmtUser.setInt(6, newUser.getDni());
-            int rowsInsertedUser = stmtUser.executeUpdate();
-    
-            // Obtener el ID generado automáticamente
-            try (ResultSet resUser = stmtUser.getGeneratedKeys()) {
-                if (resUser.next()) {
-                    int userId = resUser.getInt(1); // Obtiene el ID generado
-    
-                    // Inserta la contraseña con el ID de usuario
-                    stmPws.setInt(1, userId);
-                    stmPws.setString(2, newUser.getPassword());
-                    int rowsInsertedPws = stmPws.executeUpdate();
-    
-                    if (rowsInsertedUser > 0 && rowsInsertedPws > 0) {
-                        System.out.println("¡Usuario creado exitosamente!");
-                    }
-                } else {
-                    throw new SQLException("Error al obtener el ID del usuario insertado.");
-                }
+            stmtUser.setString(7, newUser.getPassword());
+            int row = stmtUser.executeUpdate();    
+            if (row > 0){
+                System.out.println("------Usuario creado correctamente------");
             }
         } catch (SQLException e) {
-            System.err.println("Error al insertar los datos: " + e.getMessage());
+            System.err.println("Error al crear el usuario: " + e.getMessage());
         } finally {
             db.closeConectionDB();
         }
@@ -67,7 +49,7 @@ public class userControls {
         //Inicializo una conección con la ruta de variables a implementar
         ConectionDB db = new ConectionDB();
         //Head de la tabla
-        System.out.printf("%-3s %-20s %-15s %-15s %-20s", "ID","Nombre", "Apellido", "DNI", "Email");
+        System.out.printf("%-3s %-20s %-15s %-15s %-20s %-30s %-20s", "ID","Nombre", "Apellido", "DNI", "Pais", "Ciudad","Email");
         System.out.println("");
         //Manejo de la respuesta
         try (ResultSet resultSet = db.executeQuery(query)) {
@@ -76,8 +58,10 @@ public class userControls {
                 String name = resultSet.getString("name");
                 String lastName = resultSet.getString("last_name");
                 String dni = resultSet.getString("dni");
+                String country = resultSet.getString("country");
+                String city = resultSet.getString("city");
                 String email = resultSet.getString("email");
-                String output = String.format("%-3s %-20s %-15s %-15s %-20s", id, name, lastName, dni, email);
+                String output = String.format("%-3s %-20s %-15s %-15s %-20s %-30s %-20s", id, name, lastName, dni, country, city ,email);
                 System.out.println(output);
             }
         } catch (Exception e) {
@@ -120,7 +104,7 @@ public class userControls {
             if (rowsDeleted > 0) {
                 System.out.println("¡Usuario eliminado exitosamente!");
             } else {
-                System.out.println("No se encontró un usuario con el DNI especificado.");
+                System.out.println("No se encontró un usuario con el ID especificado");
             }
         } catch (SQLException e) {
             System.err.println("Error al eliminar el usuario: " + e.getMessage());
@@ -129,7 +113,94 @@ public class userControls {
         }
     }
 
+    public static void updateUser(int id){
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        ConectionDB db = new ConectionDB();
+        boolean bandera = true;
+        while (bandera) {
+            System.out.println("Seleccione el dato que desea actualizar:");
+            System.out.println("1 - Nombre");
+            System.out.println("2 - Apellido");
+            System.out.println("3 - DNI");
+            System.out.println("4 - Email");
+            System.out.println("5 - País");
+            System.out.println("6 - Ciudad");
+            System.out.println("7 - Contraseña");
+            System.out.println("8 - Salir");
+
+            int option;
+            while (true) {
+                System.out.print("Ingrese una opción: ");
+                try {
+                    option = scanner.nextInt();
+                    break; // Sale del bucle si la entrada es numérica
+                } catch (Exception e) {
+                    System.out.println("Ingrese un valor numérico [1 - 8]");
+                    scanner.nextLine(); // Limpia el buffer del scanner
+                }
+            }
+            scanner.nextLine();     
+            
+            switch (option) {
+                case 1:
+                    System.out.print("Ingrese el nuevo nombre: ");
+                    String nombre = scanner.nextLine();
+                    String query = "UPDATE users SET name = ? WHERE id = ?"; 
+                    // Elimina la coma y corrige "UPDATE FROM"
+                    try (PreparedStatement stmtUser = db.executeChange(query)) {
+                        stmtUser.setString(1, nombre);
+                        stmtUser.setInt(2, id);
+                        stmtUser.executeUpdate();
+                        System.out.println("Actualización exitosa.");
+                    } catch (SQLException e) {
+                        System.out.println("Error de actualización: " + e.getMessage());
+                    }
+                    break;
+                case 2:
+                    System.out.print("Ingrese el nuevo apellido: ");
+                    String apellido = scanner.nextLine();
+                    
+                    break;
+                case 3:
+                    System.out.print("Ingrese el nuevo DNI: ");
+                    int dni = scanner.nextInt();
+                    scanner.nextLine(); // Limpiar el salto de línea
+                    
+                    break;
+                case 4:
+                    System.out.print("Ingrese el nuevo email: ");
+                    String email = scanner.nextLine();
+                    
+                    break;
+                case 5:
+                    System.out.print("Ingrese el nuevo país: ");
+                    String pais = scanner.nextLine();
+                    
+                    break;
+                case 6:
+                    System.out.print("Ingrese la nueva ciudad: ");
+                    String ciudad = scanner.nextLine();
+                    
+                    break;
+                case 7:
+                    System.out.print("Ingrese la nueva contraseña: ");
+                    String contrasena = scanner.nextLine();
+                    
+                    break;
+                case 8:
+                    bandera = false;
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+                    break;
+            }
+        }
+        scanner.close();
+    }
+
     public static void main(String[] args) {
-        userControls.createUser();
+        userControls.updateUser(11);
     }
 } 
